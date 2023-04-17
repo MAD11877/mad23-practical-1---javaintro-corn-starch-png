f24b5794d3f1a0d33e54e8a2b4902e8e1f0550e4
corn-starch-png <130946987+corn-starch-png@users.noreply.github.com>
2023-04-17 11:40:11 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,11 +2,11 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    /**
+    /*
      * Print "Hello World!"
      * 
      * Hint: System.out.println(...)
      */
-    system.out.println('Hello World!');
+    system.out.println("Hello World!");
   }
 }
