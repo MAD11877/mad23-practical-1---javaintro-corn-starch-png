9691f3330d8dee17c4975cfac34260372e8782f7
corn-starch-png <130946987+corn-starch-png@users.noreply.github.com>
2023-04-17 22:51:24 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -13,7 +13,7 @@ public class Question2
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
     
-    Scanner w = new Scanner(System.in);
+    Scanner w = new Scanner(System.w);
     double weight = w.nextDouble();
     
     System.out.println(weight/(height*height));
